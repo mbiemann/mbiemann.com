e194ad63a8940fc7ffb5a5f690a4e8b9ac09b823
Marcell Biemann <mbiemann@gmail.com>
2024-07-11 07:01:05 +0100

    pos

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 document.documentElement.setAttribute("data-bs-theme", "dark")
 
 import { StrictMode } from "react";
-import { Container, Row, Col, Card, Stack, ListGroup } from 'react-bootstrap';
-import { Github, Linkedin } from 'react-bootstrap-icons';
+import { Container, Row, Col, Card, Stack, ListGroup, Button } from 'react-bootstrap';
+import { Github, Linkedin, CCircle } from 'react-bootstrap-icons';
 import { createRoot } from "react-dom/client";
 
 function App() {
@@ -13,12 +13,19 @@ function App() {
         <Col md={{ span: 6, offset: 6 }}>
           <Card className="h-100 p-3">
             <Card.Body>
-              <Stack gap={3}>
-                <h1>Marcell Biemann</h1>
+              <Stack gap={3} className="h-100">
+                <h1 className="mt-auto">Marcell Biemann</h1>
                 <ListGroup>
                   <ListGroup.Item action href="https://github.com/mbiemann"><Github size={25} className="me-2" />mbiemann</ListGroup.Item>
                   <ListGroup.Item action href="https://www.linkedin.com/in/mbiemann/"><Linkedin size={25} className="me-2" />in/mbiemann</ListGroup.Item>
                 </ListGroup>
+                <Container className="mt-auto fw-light text-end">
+                  <a className="text-white-50 link-underline link-underline-opacity-0" href="https://github.com/mbiemann/mbiemann.com">
+                    mbiemann
+                    <CCircle className="mx-2" />
+                    2024
+                  </a>
+                </Container>
               </Stack>
             </Card.Body>
           </Card>
